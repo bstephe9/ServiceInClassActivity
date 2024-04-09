6d90b7e59140f9d9e974c51199e8b59aafaa1f8a
Ben Stephenson <tun78906@temple.edu>
2024-04-09 11:21:53 -0400

    Create ServiceConnection object

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,10 +1,32 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.os.IBinder
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnected = false
+
+    /**
+     * Create ServiceConnection object.
+     */
+    val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            TODO("Not yet implemented")
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
