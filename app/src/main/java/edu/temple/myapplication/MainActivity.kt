cd1c2f68e8127eb8f37e5ec92f614cb4616a4298
Ben Stephenson <tun78906@temple.edu>
2024-04-09 11:26:02 -0400

    Add button functionalities

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -39,15 +39,15 @@ class MainActivity : AppCompatActivity() {
         )
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
-
+            if (isConnected) timerBinder.start(10)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if (isConnected) timerBinder.pause()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if (isConnected) timerBinder.stop()
         }
     }
 }
